591643a607648dc358718472bddece0286008fed
iyb-wangyue <wangyue@iyunbao.com>
2023-04-12 14:03:54 +0800

    1

--- js面试/Object.js
+++ js面试/Object.js
@@ -29,16 +29,139 @@ console.log(obj[a]) // 王一
 */
 
 // 对象被转换成字符串 {}.toString() => '[object Object]'
-let obj = {},
-  a = {
-    m: '1'
-  },
-  b = {
-    n: '2'
-  };
+// let obj = {},
+//   a = {
+//     m: '1'
+//   },
+//   b = {
+//     n: '2'
+//   };
 
-obj[a] = '王一';
-obj[b] = 'wangyi';
+// obj[a] = '王一';
+// obj[b] = 'wangyi';
 
-console.log(obj[a]) // wangyi
-console.dir(obj) // { '[object Object]': 'wangyi' }
+// console.log(obj[a]) // wangyi
+// console.dir(obj) // { '[object Object]': 'wangyi' }
+
+
+// ! Object (分为 : 属性、 原型方法、实例方法)
+// ! 属性 :
+// * 1.Object.prototype.writable : 默认为false
+
+// * 2.Object.prototype.enumerable : 默认为false
+
+// * 3.Object.prototype.configurable : 默认为false
+
+// * 4.Object.prototype.constructor :用于创建一个对象的原型
+
+// ! 原型方法 :
+// * 1.Object.prototype.hasOwnProperty() : 是否包含某个属性,不来自于原型
+
+// * 2.Object.prototype.IfPrototypeOf() : 在原型链中是否包含某个属性
+
+// * 3.Object.prototype.propertylsEnumerable() : 判断指定属性是否可枚举
+
+// * 4.Object.prototype.toString() : 返回对象的字符串表示
+
+// * 5.Object.prototype.watch() : 给对象的某个属性增加监听
+
+// * 6.Object.prototype.unwatch() : 移除对象某个属性的监听
+
+// * 7.Object.prototype.valueOf() : 返回指定对象的原始值
+
+// ! 实例方法:
+// * Object.create(pro,obj) : 创建一个对象
+
+// * Object.assign(target,...obj) : 浅拷贝
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+const returnedTarget = Object.assign(target, source);
+console.log(target); // expected output: Object { a: 1, b: 4, c: 5 }
+console.log(returnedTarget); // expected output: Object { a: 1, b: 4, c: 5 }
+
+// * Object.defineProperty(obj, prop, descriptor) : 添加或修改一个自有属性并返回对象
+// const object1 = {};
+// Object.defineProperty(object1, 'property1', {
+//   value: 42,
+//   writable: false
+// });
+// object1.property1 = 77; // throws an error in strict mode
+// console.log(object1.property1); // expected output: 42
+
+
+// * Object.getOwnPropertyDescriptor(obj, prop) : 返回属性描述
+
+// * Object.defineProperties(obj, props) : 添加或修改多个自有属性并返回对象
+// var obj = {};
+// Object.defineProperties(obj, {
+//   'property1': {
+//     value: true,
+//     writable: true
+//   },
+//   'property2': {
+//     value: 'Hello',
+//     writable: false
+//   }
+//   // etc. etc.
+// });
+
+// * Object.freeze(obj) : 冻结对象,对象不可改变
+
+// * Object.entries(obj) : 返回对象键值对数据
+
+// * Object.getOwnPropertyNames(obj) : 返回属性名构成的数组
+
+// * Object.getPrototypeOf(obj) : 返回对象原型
+
+// * Object.is(value1, value2) : 判断两个字是否相等,约等于 ===
+// Object.is('foo', 'foo');     // true
+// Object.is(window, window);   // true
+
+// Object.is('foo', 'bar');     // false
+// Object.is([], []);           // false
+
+// var foo = { a: 1 };
+// var bar = { a: 1 };
+// Object.is(foo, foo);         // true
+// Object.is(foo, bar);         // false
+
+// Object.is(null, null);       // true
+
+// // 特例
+// Object.is(0, -0);            // false
+// Object.is(-0, -0);           // true
+// Object.is(NaN, 0/0);         // true
+
+// * Object.values() 返回一个给定对象自身的所有可枚举属性值的数组，值的顺序与使用for...in循环的顺序相同 ( 区别在于 for-in 循环枚举原型链中的属性 )
+// var obj = { foo: 'bar', baz: 42 };
+// console.log(Object.values(obj)); // ['bar', 42]
+
+// var obj = { 0: 'a', 1: 'b', 2: 'c' };
+// console.log(Object.values(obj)); // ['a', 'b', 'c']
+
+// var an_obj = { 100: 'a', 2: 'b', 7: 'c' };
+// console.log(Object.values(an_obj)); // ['b', 'c', 'a']
+
+// var my_obj = Object.create({}, { getFoo: { value: function() { return this.foo; } } });
+// my_obj.foo = 'bar';
+// console.log(Object.values(my_obj)); // ['bar']
+
+// console.log(Object.values('foo')); // ['f', 'o', 'o']
+
+// * Object.keys()  会返回一个由一个给定对象的自身可枚举属性组成的数组，数组中属性名的排列顺序和正常循环遍历该对象时返回的顺序一致 
+// var arr = ['a', 'b', 'c'];
+// console.log(Object.keys(arr)); // console: ['0', '1', '2']
+
+// var obj = { 0: 'a', 1: 'b', 2: 'c' };
+// console.log(Object.keys(obj)); // console: ['0', '1', '2']
+
+// var anObj = { 100: 'a', 2: 'b', 7: 'c' };
+// console.log(Object.keys(anObj)); // console: ['2', '7', '100']
+
+// var myObj = Object.create({}, {
+//   getFoo: {
+//     value: function () { return this.foo; }
+//   }
+// });
+// myObj.foo = 1;
+// console.log(Object.keys(myObj)); // console: ['foo']
--- js面试/array.js
+++ js面试/array.js
@@ -1,1 +1,149 @@
-// slice(start, end)
+// 数组的方法
+// var arr = [1, 3, 2, 4, 5];
+// var arr1 = [6, 7, 8, 9];
+
+// ! 改变原数组的方法有 shift unshift  pop push reverse sort splice fill
+
+// * 1.concat()  连接两个或更多的数组，并返回结果。原数组不变
+// arr.concat(arr1)  // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// * 2.join()    把数组的所有元素放入一个字符串。元素通过指定的分隔符进行分隔。原数组不变
+// arr.join("-") // "1-2-3-4-5"
+
+// * 3.pop()    删除并返回数组的最后一个元素。。原数组改变
+// arr.pop() //5
+
+// * 4.push()    向数组的末尾添加一个或更多元素，并返回新的长度。原数组改变
+// arr.push(5) //5
+
+// * 5.reverse()    颠倒数组中元素的顺序。原数组改变
+// arr.reverse()  //(5) [5, 4, 3, 2, 1]
+
+// * 6.shift()    删除并返回数组的第一个元素.。。原数组改变
+// arr.shift()  //1
+
+// * 7.unshift()    向数组的开头添加一个或更多元素，并返回新的长度。原数组改变
+// arr.unshift(1) //5
+
+// * 8.slice(start, end)    从某个已有的数组返回选定的元素。。
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+// console.log(animals.slice(2)); // ["camel", "duck", "elephant"]
+// console.log(animals.slice(2, 4)); // ["camel", "duck"]
+// console.log(animals.slice(1, 5)); // ["bison", "camel", "duck", "elephant"]
+// console.log(animals.slice(-2)); // ["duck", "elephant"]
+// console.log(animals.slice(2, -1)); // ["camel", "duck"]
+// console.log(animals.slice()); // ["ant", "bison", "camel", "duck", "elephant"]
+
+// * 9.sort()    对数组的元素进行排序
+// console.log(arr.sort()) // [ 1, 2, 3, 4, 5 ]
+
+// function compareFn(a, b) {
+// > 0	a 在 b 后
+// < 0	a 在 b 前
+// === 0	保持 a 和 b 的顺序
+
+// 在某些排序规则中，a 小于 b
+//   if (a<b) {
+//     return -1;
+//   }
+// 在这一排序规则下，a 大于 b
+//   if (a>b) {
+//     return 1;
+//   }
+// a 一定等于 b
+//   return 0;
+// }
+// console.log(arr.sort(compareFn)) // [ 1, 2, 3, 4, 5 ]
+// console.log(arr.sort((a,b) => a-b)) // [ 1, 2, 3, 4, 5 ]
+
+// * 10.splice(start, deleteCount, item1, item2, itemN)    删除元素，并向数组添加新元素。
+// let arr = [1, 2, 3, 4, 5];
+// let arr1 = arr.splice(2, 0 'haha')
+// let arr2 = arr.splice(2, 3)
+// let arr1 = arr.splice(2, 1, 'haha')
+// console.log(arr1) //[1, 2, 'haha', 3, 4, 5]新增一个元素
+// console.log(arr2) //[1, 2] 删除三个元素
+// console.log(arr3) //[1, 2, 'haha', 4, 5] 替换一个元素
+// console.log(arr); // [ 1, 2, 'haha', 4, 5 ]
+
+
+// * 12.toString()    把数组转换为字符串，并返回结果。原数组不变
+// arr.toString() //"1,2,3,4,5"
+
+// * 13. toLocaleString()    把数组转换为本地数组，并返回结果。原数组不变
+// var a = arr.toLocaleString() //"1,2,3,4,5"
+// console.log(a);
+
+// * 14.valueOf()    返回数组对象的原始值
+// var arr = [1,null,3,4,5]; 
+// console.log(arr.valueOf()); // [ 1, null, 3, 4, 5 ]
+
+// * 15. Array.map() 此方法是将数组中的每个元素调用一个提供的函数，结果作为一个新的数组返回，并没有改变原来的数组
+// let arr = [1, 2, 3, 4, 5]
+// let newArr = arr.map(x => x*2)
+//arr= [1, 2, 3, 4, 5]   原数组保持不变
+//newArr = [2, 4, 6, 8, 10] 返回新数组
+
+
+
+
+// * 16. Array.forEach() 此方法是将数组中的每个元素执行传进提供的函数，没有返回值，直接改变原数组，注意和map方法区分
+
+// let arr = [1, 2, 3, 4, 5]
+//    num.forEach(x => x*2)
+// arr = [2, 4, 6, 8, 10]  数组改变,注意和map区分
+
+
+
+
+// * 17.Array.filter()  此方法是将所有元素进行判断，将满足条件的元素作为一个新的数组返回
+
+// let arr = [1, 2, 3, 4, 5]
+// const isBigEnough = value => value >= 3
+// let newArr = arr.filter(isBigEnough )
+// newNum = [3, 4, 5] 满足条件的元素返回为一个新的数组
+
+
+// * 18 Array.every() 此方法是将所有元素进行判断返回一个布尔值，如果所有元素都满足判断条件，则返回true，否则为false：
+// let arr = [1, 2, 3, 4, 5]
+// const isLessThan4 => value => value < 4
+// const isLessThan6 => value => value < 6
+// arr.every(isLessThan4 ) //false
+// arr.every(isLessThan6 ) //true
+
+
+// * 19 Array.some() 此方法是将所有元素进行判断返回一个布尔值，如果存在元素都满足判断条件，则返回true，若所有元素都不满足判断条件，则返回false：
+// let arr= [1, 2, 3, 4, 5]
+// const isLessThan4 => value => value < 4
+// const isLessThan6 => value => value > 6
+// arr.some(isLessThan4 ) //true
+// arr.some(isLessThan6 ) //false
+
+// * 20.Array.reduce() 此方法是所有元素调用返回函数，返回值为最后结果,传入的值必须是函数类型：
+// let arr = [1, 2, 3, 4, 5]
+// const add = (a, b) => a + b
+// let sum = arr.reduce(add)
+// sum = 15  相当于累加的效果
+// 与之相对应的还有一个 Array.reduceRight() 方法，区别是这个是从右向左操作的
+
+// * 21. Array.isArray() 判断一个对象是不是数组，返回的是布尔值
+// arr.isArray() // true
+
+// * 22.Array.from( 需要转换为数组的对象，（映射函数）调用的函数， 映射函数中的this对象 )
+//  var arr = Array.from([1, 2, 3], x => x * 10);
+// arr[0] == 10; 
+// arr[1] == 20; 
+// arr[2] == 30;
+
+// * 23 fill(value, start, end) 数组填充
+// let array1 = [1, 2, 3, 4];
+// console.log(array1.fill(0, 2, 4)); // [1, 2, 0, 0]
+// // console.log(array1.fill(5, 1)); // [1, 5, 5, 5]
+// console.log(array1.fill(6)); // [6, 6, 6, 6]
+// console.log(array1) // [6, 6, 6, 6]
+
+// * 23 copyWithin(target, start, end) 方法浅复制数组的一部分到同一数组中的另一个位置，并返回它，不会改变原数组的长度。
+// const array1 = ['a', 'b', 'c', 'd', 'e'];
+// console.log(array1.copyWithin(0, 3, 4)); // ["d", "b", "c", "d", "e"]
+// console.log(array1.copyWithin(1, 3)); //  ["d", "d", "e", "d", "e"]
+// console.log(array1) // [ 'd', 'b', 'c', 'd', 'e' ]
